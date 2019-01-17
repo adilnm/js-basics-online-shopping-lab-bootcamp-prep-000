b8310c5821de255625758253bde734dd62a58832
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-17 06:41:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,5 +59,5 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber===null)
-  
+  return "Sorry, we don't have a credit card on file for you."
 }
