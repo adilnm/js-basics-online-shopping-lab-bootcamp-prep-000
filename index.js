0aaff3d4cf634accffc3a4ffe7a92afb6568be57
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-17 05:58:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,32 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  var newObj={itemName:item,itemPrice:Math.floor((Math.random() * 100) + 1)}
+ cart.push(newObj)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+
+    if (cart.length===0)
+    return `Your shopping cart is empty.`
+  if (cart.length===1)
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  if (cart.length===2)
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`  
+    if (cart.length>2)
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`
 }
 
 function total() {
   // write your code here
+  var total=0
+  for (let i=0;i<cart.length;i++)
+  {
+    total+=cart[i].itemPrice
+  }
+  return total
 }
 
 function removeFromCart(item) {
